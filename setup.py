08f1f7506f7a91b74a4d620386d3765dce651ac0
Ben Firshman <ben@firshman.co.uk>
2022-05-16 21:22:44 -0700

    0.0.1a6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="replicate",
     packages=["replicate"],
-    version="0.0.1a5",
+    version="0.0.1a6",
     description="Python client for Replicate",
     long_description=long_description,
     long_description_content_type="text/markdown",
