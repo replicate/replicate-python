2d497338cf51816f173c353ca1b70311a0da5eb8
Ben Firshman <ben@firshman.co.uk>
2022-05-16 18:36:24 -0700

    Long description
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 # !/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup(
     name="replicate",
     packages=["replicate"],
     version="0.0.1a1",
     description="Python client for Replicate",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Replicate, Inc.",
     license="BSD",
     url="https://github.com/replicate/replicate-python",
