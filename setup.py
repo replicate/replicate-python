3d8e3b4a247da2cba3cea7529e7287d06934113e
Ben Firshman <ben@firshman.co.uk>
2022-05-16 18:15:47 -0700

    Tag 0.0.1a1
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name="replicate",
     packages=["replicate"],
-    version="0.1.0",
+    version="0.0.1a1",
     description="Python client for Replicate",
     author="Replicate, Inc.",
     license="BSD",
