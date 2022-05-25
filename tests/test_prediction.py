a092643645e1d4bbca5186d6a855d4e8a3521dbd
Ben Firshman <ben@firshman.co.uk>
2022-05-25 12:48:43 -0700

    Add test for cancel()
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

---
+++ tests/test_prediction.py
@@ -0,0 +1,39 @@
+import replicate
+import responses
+from responses import matchers
+
+from .factories import create_version
+
+
+@responses.activate
+def test_cancel():
+    version = create_version()
+
+    responses.post(
+        "https://api.replicate.com/v1/predictions",
+        match=[
+            matchers.json_params_matcher({"version": "v1", "input": {"text": "world"}})
+        ],
+        json={
+            "id": "p1",
+            "version": "v1",
+            "urls": {
+                "get": "https://api.replicate.com/v1/predictions/p1",
+                "cancel": "https://api.replicate.com/v1/predictions/p1/cancel",
+            },
+            "created_at": "2022-04-26T20:00:40.658234Z",
+            "completed_at": "2022-04-26T20:02:27.648305Z",
+            "source": "api",
+            "status": "processing",
+            "input": {"text": "world"},
+            "output": None,
+            "error": None,
+            "logs": "",
+        },
+    )
+
+    prediction = replicate.predictions.create(version=version, input={"text": "world"})
+
+    rsp = responses.post("https://api.replicate.com/v1/predictions/p1/cancel", json={})
+    prediction.cancel()
+    assert rsp.call_count == 1
