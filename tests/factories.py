f080e81a7f6bd1d080a0ba6398b02c76c6c1cfcc
Ben Firshman <ben@firshman.co.uk>
2022-06-14 16:28:37 -0700

    Support list output
    The counterpart to https://github.com/replicate/cog/pull/655
    
    Backwards compatibility functions have been lifted from Replicate. Maybe
    we should make Cog the source of truth for these...
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- tests/factories.py
+++ tests/factories.py
@@ -9,13 +9,13 @@ def create_client():
     return client
 
 
-def create_version(client=None, openapi_schema=None):
+def create_version(client=None, openapi_schema=None, cog_version="0.3.0"):
     if client is None:
         client = create_client()
     version = Version(
         id="v1",
         created_at=datetime.datetime.now(),
-        cog_version="0.3.0",
+        cog_version=cog_version,
         openapi_schema=openapi_schema
         or {
             "info": {"title": "Cog", "version": "0.1.0"},
@@ -156,138 +156,31 @@ def create_version(client=None, openapi_schema=None):
 
 
 def create_version_with_iterator_output():
-    return create_version(
-        openapi_schema={
-            "info": {"title": "Cog", "version": "0.1.0"},
-            "paths": {
-                "/": {
-                    "get": {
-                        "summary": "Root",
-                        "responses": {
-                            "200": {
-                                "content": {"application/json": {"schema": {}}},
-                                "description": "Successful Response",
-                            }
-                        },
-                        "operationId": "root__get",
-                    }
-                },
-                "/predictions": {
-                    "post": {
-                        "summary": "Predict",
-                        "responses": {
-                            "200": {
-                                "content": {
-                                    "application/json": {
-                                        "schema": {
-                                            "$ref": "#/components/schemas/Response"
-                                        }
-                                    }
-                                },
-                                "description": "Successful Response",
-                            },
-                            "422": {
-                                "content": {
-                                    "application/json": {
-                                        "schema": {
-                                            "$ref": "#/components/schemas/HTTPValidationError"
-                                        }
-                                    }
-                                },
-                                "description": "Validation Error",
-                            },
-                        },
-                        "description": "Run a single prediction on the model.",
-                        "operationId": "predict_predictions_post",
-                        "requestBody": {
-                            "content": {
-                                "application/json": {
-                                    "schema": {"$ref": "#/components/schemas/Request"}
-                                }
-                            }
-                        },
-                    }
-                },
-            },
-            "openapi": "3.0.2",
-            "components": {
-                "schemas": {
-                    "Input": {
-                        "type": "object",
-                        "title": "Input",
-                        "properties": {
-                            "prompts": {
-                                "type": "string",
-                                "title": "Prompts",
-                                "default": "Cairo skyline at sunset.",
-                                "x-order": 0,
-                                "description": "text prompt",
-                            },
-                        },
-                    },
-                    "Output": {
-                        "type": "array",
-                        "items": {"type": "string"},
-                        "title": "Output",
-                    },
-                    "Status": {
-                        "enum": ["processing", "success", "failed"],
-                        "type": "string",
-                        "title": "Status",
-                        "description": "An enumeration.",
-                    },
-                    "Request": {
-                        "type": "object",
-                        "title": "Request",
-                        "properties": {
-                            "input": {"$ref": "#/components/schemas/Input"},
-                            "output_file_prefix": {
-                                "type": "string",
-                                "title": "Output File Prefix",
-                            },
-                        },
-                    },
-                    "Response": {
-                        "type": "object",
-                        "title": "Response",
-                        "required": ["status"],
-                        "properties": {
-                            "error": {"type": "string", "title": "Error"},
-                            "output": {"$ref": "#/components/schemas/Output"},
-                            "status": {"$ref": "#/components/schemas/Status"},
-                        },
-                        "description": "The status of a prediction.",
-                    },
-                    "ValidationError": {
-                        "type": "object",
-                        "title": "ValidationError",
-                        "required": ["loc", "msg", "type"],
-                        "properties": {
-                            "loc": {
-                                "type": "array",
-                                "items": {
-                                    "anyOf": [{"type": "string"}, {"type": "integer"}]
-                                },
-                                "title": "Location",
-                            },
-                            "msg": {"type": "string", "title": "Message"},
-                            "type": {"type": "string", "title": "Error Type"},
-                        },
-                    },
-                    "HTTPValidationError": {
-                        "type": "object",
-                        "title": "HTTPValidationError",
-                        "properties": {
-                            "detail": {
-                                "type": "array",
-                                "items": {
-                                    "$ref": "#/components/schemas/ValidationError"
-                                },
-                                "title": "Detail",
-                            }
-                        },
-                    },
-                }
-            },
-        }
-    )
+    version = create_version(cog_version="0.3.9")
+    version.openapi_schema["components"]["schemas"]["Output"] = {
+        "type": "array",
+        "items": {"type": "string"},
+        "title": "Output",
+        "x-cog-array-type": "iterator",
+    }
+    return version
+
+
+def create_version_with_list_output():
+    version = create_version(cog_version="0.3.9")
+    version.openapi_schema["components"]["schemas"]["Output"] = {
+        "type": "array",
+        "items": {"type": "string"},
+        "title": "Output",
+    }
+    return version
+
+
+def create_version_with_iterator_output_backwards_compatibility_0_3_8():
+    version = create_version(cog_version="0.3.8")
+    version.openapi_schema["components"]["schemas"]["Output"] = {
+        "type": "array",
+        "items": {"type": "string"},
+        "title": "Output",
+    }
+    return version
