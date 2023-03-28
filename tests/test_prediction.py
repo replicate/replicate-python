78f54ca08aa06d560b534238bb77b020f81477ae
Ben Firshman <ben@firshman.com>
2023-03-27 18:02:24 -0700

    Add support for new webhooks
    Signed-off-by: Ben Firshman <ben@firshman.com>

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,8 +1,54 @@
-import replicate
 import responses
 from responses import matchers
 
+import replicate
+
 from .factories import create_client, create_version
+
+
+@responses.activate
+def test_create_works_with_webhooks():
+    client = create_client()
+    version = create_version(client)
+
+    rsp = responses.post(
+        "https://api.replicate.com/v1/predictions",
+        match=[
+            matchers.json_params_matcher(
+                {
+                    "version": "v1",
+                    "input": {"text": "world"},
+                    "webhook": "https://example.com/webhook",
+                    "webhook_events_filter": ["completed"],
+                }
+            ),
+        ],
+        json={
+            "id": "p1",
+            "version": "v1",
+            "urls": {
+                "get": "https://api.replicate.com/v1/predictions/p1",
+                "cancel": "https://api.replicate.com/v1/predictions/p1/cancel",
+            },
+            "created_at": "2022-04-26T20:00:40.658234Z",
+            "completed_at": "2022-04-26T20:02:27.648305Z",
+            "source": "api",
+            "status": "processing",
+            "input": {"text": "world"},
+            "output": None,
+            "error": None,
+            "logs": "",
+        },
+    )
+
+    prediction = client.predictions.create(
+        version=version,
+        input={"text": "world"},
+        webhook="https://example.com/webhook",
+        webhook_events_filter=["completed"],
+    )
+
+    assert rsp.call_count == 1
 
 
 @responses.activate
