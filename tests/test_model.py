28a7d7d6fc2054bf2aaa8c522c4b141da90b3d5f
Mattt Zmuda <mattt@replicate.com>
2023-08-28 03:49:05 -0700

    Use VCR recordings for tests
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,35 +1,13 @@
-import responses
+import pytest
 
-from replicate.client import Client
+import replicate
 
 
-@responses.activate
-def test_versions():
-    client = Client(api_token="abc123")
+@pytest.mark.vcr("models-get.yaml")
+@pytest.mark.asyncio
+async def test_models_get():
+    model = replicate.models.get("stability-ai/sdxl")
 
-    model = client.models.get("test/model")
-
-    responses.get(
-        "https://api.replicate.com/v1/models/test/model/versions",
-        json={
-            "results": [
-                {
-                    "id": "v1",
-                    "created_at": "2022-04-26T19:29:04.418669Z",
-                    "cog_version": "0.3.0",
-                    "openapi_schema": {},
-                },
-                {
-                    "id": "v2",
-                    "created_at": "2022-03-21T13:01:04.418669Z",
-                    "cog_version": "0.3.0",
-                    "openapi_schema": {},
-                },
-            ]
-        },
-    )
-
-    versions = model.versions.list()
-    assert len(versions) == 2
-    assert versions[0].id == "v1"
-    assert versions[1].id == "v2"
+    assert model is not None
+    assert model.username == "stability-ai"
+    assert model.name == "sdxl"
