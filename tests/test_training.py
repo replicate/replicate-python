c11e17aa8fadb4670a3063ca907421e11d69ab9d
Mattt Zmuda <mattt@replicate.com>
2023-08-28 04:12:14 -0700

    Inject mock API token into tests
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- tests/test_training.py
+++ tests/test_training.py
@@ -8,7 +8,7 @@ input_images_url = "https://replicate.delivery/pbxt/JMV5OrEWpBAC5gO8rre0tPOyJIOk
 
 @pytest.mark.vcr("trainings-create.yaml")
 @pytest.mark.asyncio
-async def test_trainings_create():
+async def test_trainings_create(mock_replicate_api_token):
     training = replicate.trainings.create(
         "stability-ai/sdxl:a00d0b7dcbb9c3fbb34ba87d2d5b46c56969c84a628bf778a7fdaec30b1b99c5",
         input={
@@ -24,7 +24,7 @@ async def test_trainings_create():
 
 @pytest.mark.vcr("trainings-create__invalid-destination.yaml")
 @pytest.mark.asyncio
-async def test_trainings_create_with_invalid_destination():
+async def test_trainings_create_with_invalid_destination(mock_replicate_api_token):
     with pytest.raises(ReplicateException):
         replicate.trainings.create(
             "stability-ai/sdxl:a00d0b7dcbb9c3fbb34ba87d2d5b46c56969c84a628bf778a7fdaec30b1b99c5",
@@ -37,7 +37,7 @@ async def test_trainings_create_with_invalid_destination():
 
 @pytest.mark.vcr("trainings-get.yaml")
 @pytest.mark.asyncio
-async def test_trainings_get():
+async def test_trainings_get(mock_replicate_api_token):
     id = "ckcbvmtbvg6di3b3uhvccytnfm"
 
     training = replicate.trainings.get(id)
@@ -48,7 +48,7 @@ async def test_trainings_get():
 
 @pytest.mark.vcr("trainings-cancel.yaml")
 @pytest.mark.asyncio
-async def test_trainings_cancel():
+async def test_trainings_cancel(mock_replicate_api_token):
     input = {
         "input_images": input_images_url,
         "use_face_detection_instead": True,
