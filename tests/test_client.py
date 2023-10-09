1e841156c9cd175ea43a3fd99786390e46c0c3f0
Mattt Zmuda <mattt@replicate.com>
2023-10-09 04:41:08 -0700

    Ignore S105 for hard-coded test token
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,7 +9,7 @@ import pytest
 async def test_authorization_when_setting_environ_after_import():
     import replicate
 
-    token = "test-set-after-import"
+    token = "test-set-after-import"  # noqa: S105
 
     with mock.patch.dict(
         os.environ,
