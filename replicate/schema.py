d049225366995df8695648104019b5f413edb6c4
Mattt Zmuda <mattt@replicate.com>
2023-10-29 06:35:48 -0700

    Fix W0621: Redefining name 'version' from outer scope (line 3)
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- replicate/schema.py
+++ replicate/schema.py
@@ -15,12 +15,12 @@ def version_has_no_array_type(cog_version: str) -> Optional[bool]:
 
 def make_schema_backwards_compatible(
     schema: dict,
-    version: str,
+    cog_version: str,
 ) -> dict:
     """A place to add backwards compatibility logic for our openapi schema"""
 
     # If the top-level output is an array, assume it is an iterator in old versions which didn't have an array type
-    if version_has_no_array_type(version):
+    if version_has_no_array_type(cog_version):
         output = schema["components"]["schemas"]["Output"]
         if output.get("type") == "array":
             output["x-cog-array-type"] = "iterator"
