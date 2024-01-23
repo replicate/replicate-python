296180a33f7a825a6d770eb9d723654b91bcd310
John Doe <nurikk@users.noreply.github.com>
2024-01-23 05:40:39 +0300

    Add async_wait method to Prediction class (#225)
    Use non blocking wait function
    
    ---------
    
    Signed-off-by: John Doe <nurikk@users.noreply.github.com>
    Co-authored-by: Ainur Timerbaev <ainur.timerbaev@superawesome.com>
    Co-authored-by: Mattt Zmuda <mattt@replicate.com>

--- replicate/run.py
+++ replicate/run.py
@@ -80,12 +80,12 @@ async def async_run(
         )
 
     if not version and (owner and name and version_id):
-        version = Versions(client, model=(owner, name)).get(version_id)
+        version = await Versions(client, model=(owner, name)).async_get(version_id)
 
     if version and (iterator := _make_output_iterator(version, prediction)):
         return iterator
 
-    prediction.wait()
+    await prediction.async_wait()
 
     if prediction.status == "failed":
         raise ModelError(prediction.error)
