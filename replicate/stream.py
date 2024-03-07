145e6604f7d01548e1fc62910a714719718ee2a4
Nathan Raw <nxr9266@g.rit.edu>
2024-03-07 04:26:10 +0000

    :bug: Remove call to removeprefix to fix python 3.8

--- replicate/stream.py
+++ replicate/stream.py
@@ -114,7 +114,7 @@ class EventSource:
                 return None
 
             fieldname, _, value = line.partition(":")
-            value = value.removeprefix(" ")
+            value = value[1:] if value.startswith(" ") else value
 
             if fieldname == "event":
                 if event := ServerSentEvent.EventType(value):
